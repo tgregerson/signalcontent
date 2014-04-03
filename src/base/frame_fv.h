eb5ccb5ce45708a7329599ea11012c7815d640db
Tony Gregerson <tony.gregerson@gmail.com>
2014-04-03 10:08:05 -0500

    Add FrameDeque

--- src/base/frame_fv.h
+++ src/base/frame_fv.h
@@ -13,6 +13,9 @@
 #define SIGNAL_CONTENT_BASE_FRAME_FV_H_
 
 #include <array>
+#include <deque>
+#include <queue>
+#include <vector>
 
 #include "four_value_logic.h"
 #include "queue_fv.h"
@@ -31,11 +34,15 @@ using FrameFv = std::array<FourValueLogic, FRAME_SIZE>;
 template <size_t FRAME_SIZE>
 using FrameQueue = std::queue<FrameFv<FRAME_SIZE>>;
 
+// A FrameDeque is a deque of frames, where each frame is the same size.
+template <size_t FRAME_SIZE>
+using FrameDeque = std::deque<FrameFv<FRAME_SIZE>>;
+
 // A FrameVector is a vector of frames, where each frame is the same size.
 template <size_t FRAME_SIZE>
 using FrameVector = std::vector<FrameFv<FRAME_SIZE>>;
 
-// Methods for constructing FrameQueue or FrameVector from a QueueFv.
+// Methods for constructing frame containers from a QueueFv.
 // They assume the QueueFv is an R-Value, hence its contents may be
 // destroyed.
 template <size_t FRAME_SIZE>
@@ -54,6 +61,24 @@ FrameQueue<FRAME_SIZE> ConvertToFrameQueue(QueueFv&& q) {
     frame_queue.emplace(std::move(frame));
   }
   return frame_queue;
+}
+
+template <size_t FRAME_SIZE>
+FrameDeque<FRAME_SIZE> ConvertToFrameDeque(QueueFv&& q) {
+  FrameDeque<FRAME_SIZE> frame_deque;
+  if (q.size() % FRAME_SIZE != 0) {
+    throw std::runtime_error("Queue is not a multiple of frame size.");
+  }
+  for (size_t frame_num = 0; frame_num * FRAME_SIZE < q.size(); ++frame_num) {
+    FrameFv<FRAME_SIZE> frame;
+    for (int bit = 0; bit < FRAME_SIZE; ++bit) {
+      frame.at(bit) = q.front();
+      q.pop();
+    }
+    // Frame is destroyed by move.
+    frame_deque.emplace_back(std::move(frame));
+  }
+  return frame_deque;
 }
 
 template <size_t FRAME_SIZE>
